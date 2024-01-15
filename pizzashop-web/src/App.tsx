190a204cf92b908990ac96f0e9ceab7e14642365
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-15 19:48:46 -0300

    install react-helmet

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,14 +1,16 @@
 import "./global.css"
 
 import { RouterProvider } from 'react-router-dom'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 import { router } from "./routes"
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-     <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop"/>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
 
--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/app/dashboard'
 import { AuthLayout } from './pages/_layouts/auth'
-import { SignIn } from './pages/auth/SignIn'
+import { SignIn } from './pages/auth/signIn'
 
 
 export const router = createBrowserRouter([
