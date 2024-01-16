c4f0fb2e99cb4082f439b59ce4926ef69bd2cfeb
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-16 11:18:57 -0300

    create sign up screen

--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -3,7 +3,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/app/dashboard'
 import { AuthLayout } from './pages/_layouts/auth'
-import { SignIn } from './pages/auth/signIn'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 
 export const router = createBrowserRouter([
@@ -18,7 +19,8 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SignIn /> }
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> }
     ]
   }
 ])
