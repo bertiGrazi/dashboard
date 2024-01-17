6c06a8d0d1057140ef16f94b774f27a3e414e000
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-17 09:59:46 -0300

    create dropdown menu content

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -1,6 +1,6 @@
-import { ChevronDown } from "lucide-react";
+import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export function AccountMenu() {
   return (
@@ -11,6 +11,23 @@ export function AccountMenu() {
           <ChevronDown className="w-4 h-4"/>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Grazielli Lima Berti</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            limaberti.grazi@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2"/>
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2"/>
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
